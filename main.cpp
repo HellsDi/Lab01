eb3e3b772b530ab76418275761ef25c8c7034eee
HellsDi <000yatobog000@gmail.com>
2025-09-22 23:05:35 +0200

    Lab01

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
+
 int main(){
-    int num;
+    const float pi = 3.1415f;
+    float radius;
+    cout << "radius = ";
+    cin >> radius;
     
-    cin >> num;
-    
-    cout<<num<<endl;
+    float SurfaceArea = pow(radius,2)*4*pi;
+    float Volume = (4 * pow(radius,3)*pi)/3;
+
+    cout<< "Sarface area = "<< SurfaceArea << endl;
+    cout<< "Volume = "<< Volume << endl;
 
     return 0;
 }
