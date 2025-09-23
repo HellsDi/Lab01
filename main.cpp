e2f2506b344b160d7df98741dcd26c9df1c9c21f
HellsDi <000yatobog000@gmail.com>
2025-09-24 00:16:54 +0200

    ADD:types

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@ using namespace std;
 
 int main(){
     const float pi = 3.1415f;
+    unsigned int a;
+    cin >> a;
+    cout << a<<endl;
+    
     float radius;
     cout << "radius = ";
     cin >> radius;
@@ -12,8 +16,8 @@ int main(){
     float SurfaceArea = pow(radius,2)*4*pi;
     float Volume = (4 * pow(radius,3)*pi)/3;
 
-    cout<< "Sarface area = "<< SurfaceArea << endl;
-    cout<< "Volume = "<< Volume << endl;
+    cout<< "Sarface area = "<< SurfaceArea << " type = float, bit = 32"<< endl;
+    cout<< "Volume = "<< Volume << " type = float, bit = 32"<< endl;
 
     return 0;
 }
